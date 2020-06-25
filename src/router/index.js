4107b21652aa1b8e0d29da6caddb8635ad5863ae
Thulio Queiroz <t.queiroz@accesscredito.com.br>
2020-06-25 17:03:25 -0400

    axios e chamadas de servicos

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Hello from "../views/Hello.vue";
 
 Vue.use(VueRouter);
 
@@ -10,8 +11,9 @@ const routes = [
     name: "Home",
     component: Home
   },
+  { path: "/hello", name: "Hello", component: Hello },
   {
-    path: "/about",
+    path: "/about", 
     name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
---
+++ src/views/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Alert msg="Welcome to Your Vue.js App" />
+    <ExemploCreate msg="Welcome to Your Vue.js App" />
+    <ExemploList msg="Welcome to Your Vue.js App" />
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import Alert from "@/components/Alert.vue";
+import ExemploList from "@/components/ExemploList.vue";
+import ExemploCreate from "@/components/ExemploCreate.vue";
+
+export default {
+  name: "Hello",
+  components: {
+    HelloWorld,
+    Alert,
+    ExemploList,
+    ExemploCreate
+  }
+};
+</script>
