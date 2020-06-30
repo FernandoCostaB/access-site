9328e55f6af1ed28cb248fbcbf82b0be5e6eb342
AirtonSilveira <a.silveira@accesscredito.com.br>
2020-06-30 17:17:40 -0400

    Component Count

--- src/components/Count.vue
+++ src/components/Count.vue
@@ -11,8 +11,8 @@
                                 <span class="mbr-iconfont mbri-touch"></span>
                             </div>
                             <div class="card-text">
-                                <h3 class="count pt-3 pb-3 mbr-fonts-style display-2">1000</h3>
-                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Solicitações Feitas&nbsp;</h4>
+                                <h3 class="count pt-3 pb-3 mbr-fonts-style display-2">+ 22.000</h3>
+                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Sonhos realizados&nbsp;</h4>
                                 <p class="mbr-content-text mbr-fonts-style display-7">Praticidade e comodidade para pedir seu Empréstimo. Peça pelo computador ou celular.</p>
                             </div>
                         </div>
@@ -24,8 +24,8 @@
                             </div>
                             <div class="card-text">
                                 <h3 class="count pt-3 pb-3 mbr-fonts-style display-2">
-                              200 </h3>
-                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Análises Realizadas</h4>
+                              + 80.000.000 </h3>
+                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Capital Liberado</h4>
                                 <p class="mbr-content-text mbr-fonts-style display-7">Na AccessCrédito você conta com a facilidade, rapidez e segurança na aprovação e liberação do seu crédito.</p>
                             </div>
                         </div>
@@ -37,8 +37,8 @@
                             </div>
                             <div class="card-text">
                                 <h3 class="count pt-3 pb-3 mbr-fonts-style display-2">
-                              300 </h3>
-                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Créditos Aprovados</h4>
+                              1 </h3>
+                                <h4 class="mbr-content-title mbr-bold mbr-fonts-style display-7">Oportunidade para você agora!</h4>
                                 <p class="mbr-content-text mbr-fonts-style display-7">Descubra as vantagens de pedir um empréstimo conosco e aproveitar as menores taxas, menos burocracia e agilidade.</p>
                             </div>
                         </div>
